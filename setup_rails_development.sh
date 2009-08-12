0b2b82de65bac992f8a6ae2c82ad544d372ebcbe
Kevin Colyar <kevincolyar@ubuntu-ci.(none)>
2009-08-12 08:44:27 -0700

    Another update

--- setup_rails_development.sh
+++ setup_rails_development.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e 
 
-PACKAGES='build-essential sqlite3 libsqlite3-dev libsqlite3-ruby imagemagick libmagick9-dev ruby ruby1.8-dev libyaml-ruby libzlib-ruby rdoc vim git-core subversion rsync freetds-dev tdsodbc unixodbc unixodbc-dev libdbd-odbc-ruby libaio1'
+PACKAGES='build-essential sqlite3 libsqlite3-dev libsqlite3-ruby imagemagick libmagickwand-dev ruby ruby1.8-dev libyaml-ruby libzlib-ruby rdoc vim git-core subversion rsync freetds-dev tdsodbc unixodbc unixodbc-dev libdbd-odbc-ruby libaio1'
 
 echo "Installing apt packages"
 sudo apt-get update
@@ -11,7 +12,9 @@ wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz
 tar zxfv rubygems-1.3.1.tgz
 cd rubygems-1.3.1
 sudo ruby setup.rb
-sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
+if [! -L /usr/bin/gem]; then
+  sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
+fi 
 cd ..
 
 echo "Add github to rubygem sources"
@@ -24,6 +27,7 @@ echo "Install gems"
 GEMS='rails cgi_multipart_eof_fix cucumber daemons diff-lcs faker fastthread gem_plugin hoe memcache-client mongrel mongrel_cluster net-ping net-sftp net-ssh passenger polyglot populator rack rake randexp rmagick rspec rspec-rails ruby-net-ldap rubyforge rubygems-update sqlite3-ruby term-ansicolor treetop ZenTest'
 
 echo "Install activerecord-sqlserver-adapter"
-sudo gem install activerecord-sqlserver-adapter --source=http://gems.rubyonrails.org
+sudo gem install activerecord-sqlserver-adapter 
+#--source=http://gems.rubyonrails.org
 
 sudo gem install $GEMS
